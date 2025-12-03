0a98a65dc4ee5bb6810a94f96d055806930fbb17
Gomit Saha <gomit.dev@gmail.com>
2025-12-03 06:26:25 +0530

    Fix merge conflicts in Footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Link } from "react-router-dom";
-=======
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faTwitter, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+// <<<<<<< HEAD
+// =======
+// >>>>>>> ae7a678ae623fc9f51aaa468bae5fe2a8a7a1fe4
 
->>>>>>> ae7a678ae623fc9f51aaa468bae5fe2a8a7a1fe4
 
 const Footer = () => {
   return (
